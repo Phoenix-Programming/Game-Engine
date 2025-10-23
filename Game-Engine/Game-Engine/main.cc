4b0516659c763a065d1591e39c3a3272900555bf
Jordan Baumann <69920353+Cat-Brain@users.noreply.github.com>
2025-10-23 19:16:46 -0400

    Imported and added initialization for GLFW

--- Game-Engine/Game-Engine/main.cc
+++ Game-Engine/Game-Engine/main.cc
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
 	std::cout << "Hello world!\n";
+
+	if (!glfwInit())
+	{
+		std::cout << "Error Initializing GLFW!\n";
+		return 0;
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
