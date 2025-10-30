25f58a16f8d74b9e646ef7e04cea126e9ff54685
Jordan Baumann <69920353+Cat-Brain@users.noreply.github.com>
2025-10-30 19:41:15 -0400

    Imported and added initialization for GLFW (#19)

--- Game-Engine/Game-Engine/main.cc
+++ Game-Engine/Game-Engine/main.cc
@@ -1,18 +1,77 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+
+#pragma region Should Be Migrated To Another File
+
+GLFWwindow* window;
+int defaultScreenWidth = 800, defaultScreenHeight = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+bool InitGLFW()
+{
+	if (!glfwInit())
+	{
+		std::cout << "Error Initializing GLFW!\n";
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(defaultScreenWidth, defaultScreenHeight, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	return true;
+}
+
+void DeInitGLFW()
+{
+	glfwTerminate();
+}
+
+bool InitGLAD()
+{
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		return true;
+	
+	std::cout << "Error Initializing GLAD\n";
+	return false;
+}
+
+#pragma endregion
 
 int main()
 {
 	std::cout << "Hello world!\n";
 
-	if (!glfwInit())
+	InitGLFW();
+	InitGLAD();
+
+	// Game here
+	while (!glfwWindowShouldClose(window))
 	{
-		std::cout << "Error Initializing GLFW!\n";
-		return 0;
+		glClearColor(0.2f, 0.2f, 0.2f, 0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	glfwTerminate();
-
+	DeInitGLFW();
+	
 	return 0;
 }
