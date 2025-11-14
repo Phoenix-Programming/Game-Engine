22465b6408a85a0be75e03c5c3e7a9449f42b237
Jordan Baumann <69920353+Cat-Brain@users.noreply.github.com>
2025-11-13 19:06:06 -0500

    Implemented basic engine.h and cc (#26)
    * Mostly finished but GLAD has an error
    
    * Fixed errors with GLAD so now the program runs properly
    
    * Renamed .cpp files to .cc

--- Game-Engine/Game-Engine/main.cc
+++ Game-Engine/Game-Engine/main.cc
@@ -1,89 +1,12 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-
-#pragma region Should Be Migrated To Another File
-
-GLFWwindow* window;
-int defaultScreenWidth = 800, defaultScreenHeight = 600;
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
-
-bool InitGLFW()
-{
-	if (!glfwInit())
-	{
-		std::cout << "Error Initializing GLFW!\n";
-		return false;
-	}
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	window = glfwCreateWindow(defaultScreenWidth, defaultScreenHeight, "LearnOpenGL", NULL, NULL);
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	return true;
-}
-
-void DeInitGLFW()
-{
-	glfwTerminate();
-}
-
-bool InitGLAD()
-{
-	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-		return true;
-	
-	std::cout << "Error Initializing GLAD\n";
-	return false;
-}
-
-#pragma endregion
-
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-		// some spacebar function
-	}
-}
+#include "engine.h"
 
 int main()
 {
 	std::cout << "Hello world!\n";
 
-	InitGLFW();
-	InitGLAD();
-
-	// Game here
-	while (!glfwWindowShouldClose(window))
-	{
-		glClearColor(0.2f, 0.2f, 0.2f, 0);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-		processInput(window);
-	}
-
-	DeInitGLFW();
+	Engine engine;
+	engine.Execute();
 	
 	return 0;
 }
---
+++ Game-Engine/Game-Engine/window.cc
@@ -0,0 +1,96 @@
+#include "window.h"
+
+std::vector<Window*> Window::windows{ };
+bool Window::glfwInitialized = false, Window::gladInitialized = false;
+
+Window::Window(int width, int height) :
+	width(width), height(height)
+{
+	if (!glfwInitialized)
+		glfwInitialized = InitGLFW();
+
+	window = glfwCreateWindow(width, height, "Game Engine Project", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return;
+	}
+	windows.push_back(this);
+	glfwMakeContextCurrent(window);
+
+	if (!gladInitialized)
+		gladInitialized = InitGLAD();
+}
+
+bool Window::ShouldClose()
+{
+	return glfwWindowShouldClose(window);
+}
+
+void Window::Start()
+{
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+void Window::Update()
+{
+	glfwPollEvents();
+	glfwSwapBuffers(window);
+
+	glClearColor(0.2f, 0.2f, 0.2f, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Window::Close()
+{
+	glfwDestroyWindow(window);
+}
+
+Window& Window::GetWindow(GLFWwindow* glfwWindow)
+{
+	for (Window* window : Window::windows)
+		if (window->window == glfwWindow)
+			return *window;
+}
+
+void Window::framebuffer_size_callback(GLFWwindow* glfwWindow, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	Window& window = Window::GetWindow(glfwWindow);
+	window.width = width;
+	window.height = height;
+}
+
+bool Window::InitGLFW()
+{
+	if (!glfwInit())
+	{
+		std::cout << "Error Initializing GLFW!\n";
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	std::cout << "Successfully Initialized GLFW\n";
+	return true;
+}
+
+bool Window::InitGLAD()
+{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Error Initializing GLAD\n";
+		return false;
+	}
+
+	std::cout << "Successfully Initialized GLAD\n";
+	return true;
+}
+
+void Window::DeInitGLFW()
+{
+	glfwTerminate();
+}
